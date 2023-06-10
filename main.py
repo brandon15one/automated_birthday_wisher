820315ca84af8b6ef75292a58e7c1dea96aa220c
Siddhant Baghel <87269873+brandon15one@users.noreply.github.com>
2023-06-10 17:58:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import smtplib
-
+import datetime as dt
+import random as rndm
 
 my_email = "siddhantb151@gmail.com"
 password = "wyjiuyfuvbbuwtzh"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="siddhantb151@yahoo.com",
-        msg="Subject: hello\n\nthis is the body og my email")
+today = dt.datetime.now()
+day = today.weekday()
 
+
+def new_quote():
+    with open("quotes.txt", mode="r") as qt:
+        qt_list = qt.readlines()
+        quote = rndm.choice(qt_list)
+        return quote
+
+
+if day == 0:
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="siddhantb151@yahoo.com",
+            msg=f"Subject: MONDAY MOTIVATION \n\n{new_quote()}")
